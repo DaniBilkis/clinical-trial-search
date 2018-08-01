2b3772c7005e0fee805d441584c05fd9b7437068
Dani Bilkis <dani.bilkis@gmail.com>
2018-08-01 18:24:06 -0500

    app.js changes to remove favicon and assign dist as a static deployment directory

--- app.js
+++ app.js
@@ -40,13 +40,14 @@ if ( 'production' === app.get('env') ) {
 //app.set('views', path.join(__dirname, 'views'));
 //app.set('view engine', 'ejs');
 
-app.use(favicon(path.join(__dirname, 'dist', 'favicon.ico')));
-app.use(logger('dev'));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({
-    'extended': 'false'
-}));
-app.use(cookieParser());
+// app.use(favicon(path.join(__dirname, 'dist', 'favicon.ico')));
+app.use( logger('dev') );
+app.use( bodyParser.json() );
+app.use( bodyParser.urlencoded({ 'extended': 'false' }) );
+app.use( cookieParser() );
+
+// Create link to Angular build directory
+app.use( express.static( path.join( __dirname, 'dist' ) ) );
 
 //app.use(mongo_db());
 
